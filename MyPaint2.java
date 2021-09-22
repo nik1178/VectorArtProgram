b50362254207358da864cb9049239cbd9a74cf7d
Nik Jenic <jenic.nik@gmail.com>
2021-09-22 21:43:36 +0200

    Changed the paths to work based on a class. Added clear button.

--- MyPaint2.java
+++ MyPaint2.java
@@ -4,37 +4,49 @@ import java.awt.event.*;
 import java.awt.geom.*;
 import java.util.*;
 
-class MyPaint2 extends JFrame implements KeyListener{
-
+class MyPaint2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new MyPaint2();
+		new MyFrame();
 	}
-	DrawCanvas2 drawCanvas = new DrawCanvas2();
-	MyPaint2(){
+}
+class MyFrame extends JFrame implements KeyListener, ActionListener{
 
-		//brand new comment in my code
-		
-		this.add(drawCanvas);
-		//Testing VSGit one last time
+	
+	DrawCanvas2 drawCanvas = new DrawCanvas2();
+	JButton clearButton = new JButton("Clear");
+	JButton colorButton = new JButton();
+	MyFrame(){
+
+		drawCanvas.setBounds(0,50,500,500);
+
+		clearButton.addActionListener(
+			e -> {
+				drawCanvas.paths.clear();
+				drawCanvas.currentPath = 0;
+				repaint();
+			}
+		);
+		clearButton.setBounds(10,10,50,20);
+		clearButton.setFocusable(false);
 
 		this.addKeyListener(this);
+
+		this.add(clearButton);
+		this.add(drawCanvas);
+
 		
+		this.getContentPane().setBackground(Color.gray);
 		this.setSize(new Dimension(500,600));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		this.setLayout(new FlowLayout());
+		//this.setLayout(new FlowLayout());
+		this.setLayout(null);
 		this.setVisible(true);
 	}
 
 	//Keyboard
 	boolean ctrlKey = false;
-	boolean zKey = false;
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
@@ -42,45 +54,23 @@ class MyPaint2 extends JFrame implements KeyListener{
 			ctrlKey = true;
 		}
 		if(e.getKeyCode()==90){
-			//zKey = true;
-			if(ctrlKey/*  && zKey */){
-				/* drawCanvas.lineCoordsX1.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsX2.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsY1.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsY2.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsColor.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineNameList.remove(drawCanvas.lineNameList.size()-1);
-
-				drawCanvas.coordsX.remove(drawCanvas.dotNameList.get(drawCanvas.dotNameList.size()-1));
-				drawCanvas.coordsY.remove(drawCanvas.dotNameList.get(drawCanvas.dotNameList.size()-1));
-				drawCanvas.coordsColor.remove(drawCanvas.dotNameList.get(drawCanvas.dotNameList.size()-1));
-				drawCanvas.dotNameList.remove(drawCanvas.dotNameList.size()-1); */
-				//drawCanvas.paths.remove(--drawCanvas.currentPath);
+			if(ctrlKey){
 				if(drawCanvas.currentPath>0)
 					drawCanvas.paths.remove(--drawCanvas.currentPath);
-				//--drawCanvas.currentPath;
 				repaint();
 			}
 		}
 		if(e.getKeyCode()==89){
-			//zKey = true;
-			if(ctrlKey/*  && zKey */){
-				/* drawCanvas.lineCoordsX1.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsX2.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsY1.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsY2.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineCoordsColor.remove(drawCanvas.lineNameList.get(drawCanvas.lineNameList.size()-1));
-				drawCanvas.lineNameList.remove(drawCanvas.lineNameList.size()-1);
-
-				drawCanvas.coordsX.remove(drawCanvas.dotNameList.get(drawCanvas.dotNameList.size()-1));
-				drawCanvas.coordsY.remove(drawCanvas.dotNameList.get(drawCanvas.dotNameList.size()-1));
-				drawCanvas.coordsColor.remove(drawCanvas.dotNameList.get(drawCanvas.dotNameList.size()-1));
-				drawCanvas.dotNameList.remove(drawCanvas.dotNameList.size()-1); */
-				//drawCanvas.paths.remove(--drawCanvas.currentPath);
-				//drawCanvas.currentPath+=1;
+			if(ctrlKey){
 				repaint();
 			}
 		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
@@ -88,12 +78,15 @@ class MyPaint2 extends JFrame implements KeyListener{
 		if(e.getKeyCode()==17){
 			ctrlKey = false;
 		}
-		if(e.getKeyCode()==90){
-			zKey = false;
-		}
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
 
+//The canvas on which you draw
 class DrawCanvas2 extends JPanel implements MouseListener, MouseMotionListener{
 	DrawCanvas2(){
 		this.setPreferredSize(new Dimension(500,500));
@@ -102,54 +95,45 @@ class DrawCanvas2 extends JPanel implements MouseListener, MouseMotionListener{
 		this.setBackground(Color.black);
 	}
 	
-	/* HashMap<String, Integer> coordsX = new HashMap<>();
-	HashMap<String, Integer> coordsY = new HashMap<>();
-	HashMap<String, Color> coordsColor = new HashMap<>();
-	ArrayList<String> dotNameList = new ArrayList<>(); */
-	
-	Color paintColor = Color.white;
+	static Color paintColor = Color.white;
 	@Override
 	public void paint(Graphics g){
 		super.paint(g);
 		Graphics2D g2 = (Graphics2D) g;
+	
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+		RenderingHints.VALUE_ANTIALIAS_ON); 
+	
 		g2.setStroke(new BasicStroke(5));
-		if(started){
-			for(int i=0; i<paths.size(); i++){
-				g2.draw(paths.get(i));
-				System.out.println("test");
-			}
+		for(int i=0; i<paths.size(); i++){
+			g2.draw(paths.get(i).path);
 		}
 	}
-	boolean started = false;
-	/* HashMap<String,Integer> lineCoordsX1 = new HashMap<>();
-	HashMap<String,Integer> lineCoordsY1 = new HashMap<>();
-	HashMap<String,Integer> lineCoordsX2 = new HashMap<>();
-	HashMap<String,Integer> lineCoordsY2 = new HashMap<>();
-	HashMap<String,Color> lineCoordsColor = new HashMap<>(); */
+
+
+	ArrayList<MyPath> paths = new ArrayList<>();
+	int currentPath = 0;
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		paths.add(new MyPath(e.getX(), e.getY()));
+		paths.get(currentPath).path.moveTo(e.getX(), e.getY());
+		repaint();
+	}
 	ArrayList<String> lineNameList = new ArrayList<>();
 	int tempX;
 	int tempY;
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		paths.get(currentPath).lineTo(e.getX(), e.getY());
-		repaint();
-	}
-	ArrayList<Path2D> paths = new ArrayList<>();
-	int currentPath = 0;
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		paths.add(new Path2D.Double());
-		paths.get(currentPath).moveTo(e.getX(), e.getY());
-		started = true;
+		paths.get(currentPath).path.lineTo(e.getX(), e.getY());
 		repaint();
 	}
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		paths.get(currentPath).moveTo(e.getX(), e.getY());
-		paths.get(currentPath).closePath();
+		paths.get(currentPath).path.moveTo(e.getX(), e.getY());
+		paths.get(currentPath).path.closePath();
 		currentPath++;
 		repaint();
 	}
@@ -177,8 +161,15 @@ class DrawCanvas2 extends JPanel implements MouseListener, MouseMotionListener{
 		// TODO Auto-generated method stub
 		
 	}
-
-
-
-	
+}
+class MyPath {
+	Color color = DrawCanvas2.paintColor;
+	double currentX;
+	double currentY;
+	Path2D path = new Path2D.Double();
+	MyPath(double x, double y){
+		currentX = x;
+		currentY = y;
+		path.moveTo(x, y);
+	}
 }
